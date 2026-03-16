6c20d8fade1afbed5f23d3cd9d26eba40f545078
yunji cho <132728613+YUMZII@users.noreply.github.com>
2026-03-16 23:06:36 +0900

    Update ascii_unicode.java

--- java_basic/ascii_unicode.java
+++ java_basic/ascii_unicode.java
@@ -4,8 +4,8 @@ public class VariableTest1 {
 		// TODO Auto-generated method stub
 		System.out.println('1'+1); // 50: 왜냐하면 '1'은 문자가 아니라 내부적으로 숫자(ASCII/Unicode)
     //로 계산 됨 '1'은 49
-    System.out.println('A'+1); // 'A'는 65 그래서 66으로 출력
-    System.out.println((char)('1'+1)); // 49+1=50 char(50)은 '2' 그래서 2 출력
+    	System.out.println('A'+1); // 'A'는 65 그래서 66으로 출력
+    	System.out.println((char)('1'+1)); // 49+1=50 char(50)은 '2' 그래서 2 출력
   }
 
 }
