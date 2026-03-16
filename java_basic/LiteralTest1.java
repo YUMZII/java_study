f164cc2e229b237aed5f35cd80a5b166a44169d3
yunji cho <132728613+YUMZII@users.noreply.github.com>
2026-03-16 23:10:48 +0900

    Create LiteralTest1.java

---
+++ java_basic/LiteralTest1.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class LiteralTest3 {
+	public static void main(String[] args) {
+		System.out.println("123456789t123456789t123456789");
+		System.out.println("123456789\t123456789\t123456789");// 열 맞추기에 사용됨
+		System.out.println("123456789n123456789n123456789");
+		System.out.println("123456789\n123456789\n123456789");// 줄바꿈 
+		System.out.println("123456789\n\n123456789\n\n123456789"); // 두 줄 띄기
+		System.out.println("\n\n\n"); //줄 3개를 띄움 그러니까 총 4줄 띄고 출력
+		System.out.println('가');
+		System.out.println('나');
+		System.out.println('다');
+		System.out.println("\n\n\n");
+		System.out.print('가');
+		System.out.print('나');
+		System.out.print('다');
+	}
+}
